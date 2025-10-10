2e124dbd1ae6f148b9b06eb7b2ab55eebebdbf9e
kfir hayman <kfir.hayman@gmail.com>
2025-10-10 20:49:46 +0300

    more script work - push for colab

--- controlnet_params.py
+++ controlnet_params.py
@@ -10,9 +10,10 @@ from model.utils import fix_len_compatibility
 
 
 # data parameters
-train_filelist_path = 'resources/filelists/ljspeech/train.txt'
-valid_filelist_path = 'resources/filelists/ljspeech/valid.txt'
-test_filelist_path = 'resources/filelists/ljspeech/test.txt'
+filelist_dir = 'resources/filelists/ljspeech/'
+train_filelist_path = filelist_dir +'train.txt'
+valid_filelist_path = filelist_dir + 'valid.txt'
+test_filelist_path = filelist_dir + 'test.txt'
 cmudict_path = 'resources/cmu_dictionary'
 add_blank = True
 n_feats = 80
--- grad_tts_setup.py
+++ grad_tts_setup.py
@@ -10,25 +10,13 @@ torchaudio
 import os
 from fix_path_to_dataset import generate_filelist, get_audio_dir
 
-# if not os.path.exists('./model/'):
-#     raise FileNotFoundError("Directory ./model does not exist, cannot setup model/monotonic_align")
-# if not os.path.exists('./model/monotonic_align'):
-#     raise FileNotFoundError("Directory ./model/monotonic_align does not exist, cannot setup model/monotonic_align")
-#
-# if not os.path.exists('./model/monotonic_align/model'):
-#     os.mkdir('./model/monotonic_align/model')
-#
-# if not os.path.exists('./model/monotonic_align/model/monotonic_align'):
-#     os.mkdir('./model/monotonic_align/model/monotonic_align')
 
 # create appropriate dataset
 from pathlib import Path
+from controlnet_params import filelist_dir
 audio_dir = get_audio_dir()
-from controlnet_params import train_filelist_path, valid_filelist_path, test_filelist_path
-file_dirs = [train_filelist_path, valid_filelist_path, test_filelist_path]
-for d in file_dirs:
-    # print(f"🔧 {d} -> {d}_fixed.txt", Path(d).resolve())
-    generate_filelist(Path(d).resolve(), audio_dir)
+
+generate_filelist(Path(filelist_dir).resolve(), audio_dir)
 
 # python inference.py -f ./resources/filelists/synthesis.txt -c ./checkpts/grad-tts.pt
 
